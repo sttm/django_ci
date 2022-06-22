0cdcba7fe70dfaf2842c1035d0e31ad3d488806c
sttm <sttm@DESKTOP-LG7SIN6.localdomain>
2022-06-22 23:08:12 +0300

    test + firstpage

---
+++ firstpage/tests.py
@@ -0,0 +1,11 @@
+from urllib import response
+from django.test import Client, TestCase
+
+class IndexTestCase(TestCase):
+    def setUp(self) -> None:
+        self.client = Client()
+
+    def test_get_index_endpoint(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content, b'Hello world')
